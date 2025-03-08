81ca4ed3f86eac797dc47515250600835ed18336
Dallas Kidd <4213800+dallasbrittany@users.noreply.github.com>
2025-03-08 12:10:24 -0600

    Adjustments to setting how many days to search for

--- main.py
+++ main.py
@@ -4,17 +4,18 @@ from searches.url_filter import UrlFilterCrawler
 
 def main(crawler: str):
     max_articles = None # unlimited with None
+    days_back = 7
+
     max_str = f" with max articles set to {max_articles}" if max_articles else " with no max article limit"
-    print(f"Using {crawler} crawler for search{max_str}.\n")
+    print(f"Using {crawler} crawler for search{max_str} and going {days_back} day(s) back.\n")
     if crawler == "filter":
         body_search_terms = ["pollution", "climate crisis", "environmental"]
-        days_back = 1
         crawler = BodyFilterCrawler(max_articles, body_search_terms, days_back)
         crawler.run_crawler()
     elif crawler == "url":
         filter_out_terms = ["advertisement", "podcast"]
         filter_include_terms = ["coral", "climate"]
-        url_filter_crawler = UrlFilterCrawler(max_articles, filter_out_terms, filter_include_terms)
+        url_filter_crawler = UrlFilterCrawler(max_articles, filter_out_terms, filter_include_terms, days_back)
         url_filter_crawler.run_crawler()
     else:
         raise ValueError(f"Unknown crawler type: {crawler}")
--- searches/url_filter.py
+++ searches/url_filter.py
@@ -1,14 +1,17 @@
 from typing import List
+import datetime
 from fundus import Crawler, PublisherCollection
 from fundus.scraping.filter import inverse, regex_filter, lor, land
 from searches.helpers import display1
 
 class UrlFilterCrawler:
-    def __init__(self, max_articles: int, filter_out_terms: str, filter_include_terms: List[str]):
+    def __init__(self, max_articles: int, filter_out_terms: str, filter_include_terms: List[str], days: int):
         self.crawler = Crawler(PublisherCollection.us)
         self.max_articles = max_articles
         self.filter_out_terms_list = filter_out_terms
         self.filter_include_terms_list = filter_include_terms
+        self.days = days
+        self.end_date = datetime.date.today() - datetime.timedelta(days=days)
 
     def run_crawler(self):
         filter_out_terms = "|".join(self.filter_out_terms_list)
@@ -18,4 +21,10 @@ class UrlFilterCrawler:
         filter_include = inverse(land(*filter_include_terms))
 
         for article in self.crawler.crawl(max_articles=self.max_articles, url_filter=lor(filter_out, filter_include)):
-            display1(article)
+            # just not printing the ones with unwanted dates is a workaround
+            if article.publishing_date.date() > self.end_date:
+                display1(article)
+            elif self.max_articles:
+                # because of the workaround with filtering out dates, the max article count likely won't match the number of found articles, so printing this is helpful in that case
+                print("\n(Skipping display of older article.)")
+                print("-"*20)
